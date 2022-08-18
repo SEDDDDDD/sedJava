9cb7ae66ab96615b0ff5d8e1383587dfbf6493a7
SEDDDDDD <fdrdcd@gmail.com>
2022-08-18 17:35:29 +0900

    0818

--- src/javabook2/ch02/ByteExample.java
+++ src/javabook2/ch02/ByteExample.java
@@ -1,12 +1,17 @@
 package javabook2.ch02;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ByteExample {
 
 	public static void main(String[] args) {
-		byte var1 = -128;
-		short s = 32767;
-		int i = 2147483647;
-		long l = 21351223513L;
+		String id = "흐하흐하";
+		
+		List<String> idList = new ArrayList<String>();
+		idList.add(id);
+		
+		System.out.println(idList.contains(id));
 		
 		
 		
